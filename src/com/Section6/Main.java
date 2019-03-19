4bd73739463402e1013a493e37bcaf417b5ff43b
Bobby Smalls <bsmalls05@hotmail.com>
2019-03-19 19:17:24 -0400

    Initial commit

---
+++ src/com/Section6/Main.java
@@ -0,0 +1,14 @@
+package com.Section6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MustangGT mustangGT = new MustangGT(36);
+        mustangGT.steer(50);
+        mustangGT.accelerate(30);
+        mustangGT.accelerate(10);
+        mustangGT.accelerate(10);
+        mustangGT.accelerate(20);
+
+    }
+}
